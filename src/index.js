e0d8516aaaa2c891ede460e7f47041ae598a2684
Oliver Smit <olmesm@gmail.com>
2019-05-23 13:43:55 +0100

    added violationCallback

--- src/index.js
+++ src/index.js
@@ -1,52 +1,55 @@
+/* global cy, assert, Cypress */
+
+let violationsFunc = violations => {
+  assert.strictEqual(
+    violations.length,
+    0,
+    `${violations.length} accessibility violation${
+      violations.length === 1 ? '' : 's'
+    } ${violations.length === 1 ? 'was' : 'were'} detected`
+  );
+};
+
 Cypress.Commands.add('injectAxe', () => {
-    cy.window({ log: false }).then(window => {
-      const axe = require('axe-core')
-      window.eval(axe.source)
+  cy.window({ log: false }).then(window => {
+    const axe = require('axe-core');
+    window.eval(axe.source);
+  });
+});
+
+Cypress.Commands.add('configureAxe', (configurationOptions = {}) => {
+  cy.window({ log: false })
+    .then(win => {
+      return win.axe.configure(configurationOptions);
+    });
+});
+
+Cypress.Commands.add('checkA11y', (context, options, violationCallback = violationsFunc) => {
+  cy.window({ log: false })
+    .then(win => {
+      if (isEmptyObjectorNull(context)) context = undefined;
+      if (isEmptyObjectorNull(options)) options = undefined;
+
+      return win.axe.run(context || win.document, options);
     })
-  })
-  
-  Cypress.Commands.add('configureAxe', (configurationOptions = {}) => {
-    cy.window({ log: false })
-      .then(win => {
-        return win.axe.configure(configurationOptions)
-      })
-  })
-  
-  Cypress.Commands.add('checkA11y', (context, options) => {
-    cy.window({ log: false })
-      .then(win => {
-        if (isEmptyObjectorNull(context)) context = undefined
-        if (isEmptyObjectorNull(options)) options = undefined
-        return win.axe.run(context ? 
-              context = context : 
-              context = win.document, options)
-      })
-      .then(({ violations }) => {
-        if (violations.length) {
-          cy.wrap(violations, { log: false }).each(v => {
-            Cypress.log({
-              name: 'a11y error!',
-              consoleProps: () => v,
-              message: `${v.id} on ${v.nodes.length} Node${
-                v.nodes.length === 1 ? '' : 's'
-              }`
-            })
-          })
-        }
-        return cy.wrap(violations, { log: false })
-      })
-      .then(violations => {
-        assert.equal(
-          violations.length,
-          0,
-          `${violations.length} accessibility violation${
-            violations.length === 1 ? '' : 's'
-          } ${violations.length === 1 ? 'was' : 'were'} detected`
-        )
-      })
-  })
-  
-  function isEmptyObjectorNull(value) {
-    if (value == null) return true
-    return Object.entries(value).length === 0 && value.constructor === Object
-  }
+    .then(({ violations }) => {
+      if (violations.length) {
+        cy.wrap(violations, { log: false }).each(v => {
+          Cypress.log({
+            name: 'a11y error!',
+            consoleProps: () => v,
+            message: `${v.id} on ${v.nodes.length} Node${
+              v.nodes.length === 1 ? '' : 's'
+            }`
+          });
+        });
+      }
+      return cy.wrap(violations, { log: false });
+    })
+    .then(violationCallback);
+});
+
+function isEmptyObjectorNull (value) {
+  if (value == null) return true;
+  return Object.entries(value).length === 0 && value.constructor === Object;
+}
